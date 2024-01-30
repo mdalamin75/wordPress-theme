39a04259c77bbb99818d21398f99314f3eadac13
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-01-30 20:41:27 +0600

    added read more button function

--- inc/default.php
+++ inc/default.php
@@ -5,3 +5,17 @@ add_theme_support('title-tag');
 // Thumbnail Image Area
 add_theme_support( 'post-thumbnails', array('page', 'post') );
 add_image_size( 'post-thumbnails', 970, 350, true);
+
+
+// Excerpt to 40 word or read more button
+
+function md_excerpt_more($more){
+    global $post;
+    return '<br> <br> <a class="readmore" href="'.get_permalink( $post->ID) .'">'. 'Read More' . '</a>';
+};
+add_filter('excerpt_more', 'md_excerpt_more');
+
+function md_excerpt_length($length){
+    return 40;
+}
+add_filter('excerpt_length', 'md_excerpt_length', 999);
