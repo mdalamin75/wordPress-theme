694cff780c8e99778d075ceeb2a97624a4261e23
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-02-02 21:23:13 +0600

    setup blog & post page and added comments section

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php
-    // The Main Template file
+    // The template for displaying pages
     get_header();
 ?>
     <section id="body_area">
         <div class="container">
             <div class="row">
                 <div class="col-md-9">
-                    <?php get_template_part('template_part/blog_setup');?>
+                    <?php get_template_part('template_part/post_setup');?>
                 </div>
                 <div class="col-md-3">
                     <?php get_sidebar();?>
