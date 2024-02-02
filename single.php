5386e84c8c701e0541d51c8d619ea4aec235a352
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-02-02 22:05:51 +0600

    added archive page

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
     <section id="body_area">
         <div class="container">
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-9 post_page">
                     <?php get_template_part('template_part/post_setup');?>
                 
                     <div id="comments_area">
