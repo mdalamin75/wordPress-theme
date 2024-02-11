f602fb06ebce3de0364c17eae8a0d825907a657d
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-02-12 05:13:55 +0600

    add category in custom post

--- inc/custom_post.php
+++ inc/custom_post.php
@@ -24,6 +24,7 @@ function custom_slider(){
         'rewrite' => array('slag' => 'slider'),
         'supports' => array ('title', 'thumbnail', 'editor'),
     ));
+    add_theme_support('post-thumbnauls');
 }
 
 add_action('init', 'custom_slider');
@@ -49,9 +50,26 @@ function custom_service(){
         'hierarchial' => true,
         'show_uo' => true,
         'capbility_type' => 'post',
+        'taxonomies' => array('category'),
         'rewrite' => array('slag' => 'service'),
         'supports' => array ('title', 'thumbnail', 'editor'),
     ));
+    add_theme_support('post-thumbnauls');
 }
 
 add_action('init', 'custom_service');
+
+function query_post_type($query){
+    if (is_category()) {
+        $post_type = get_query_var('post_type');
+        if ($post_type) {
+            $post_type = $post_type;
+        } else {
+            $post_type = array('post', 'service');
+            $query -> set('post_type', $post_type);
+            return $query;
+        }
+    }
+    
+}
+add_filter('pre_get_posts', 'query_post_type');
